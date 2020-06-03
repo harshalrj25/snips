3df51d516f35ed9a5d31b52953ca0ca62a2f6429
harshal jadhav <harshalrj25@gmail.com>
2020-06-03 17:29:40 +0530

    Update exportFilePath.py

--- exportFilePath.py
+++ exportFilePath.py
@@ -1,9 +1,13 @@
 import os
 import csv
 
-path = '/Users/iosdev1/youtube/exportFilePath/dummyData'
+# path format is for windows
+  path = 'D:\\foldername\\subfoldername'
+  
+# if you are on using macOS uncomment and use below path format, and comment out the above one
+# path = '/Users/iosdev1/youtube/exportFilePath/dummyData'
 
-with open('YOUR_OUTPUT_FILE_xlsx.csv', 'wb') as csvfile:
+with open('YOUR_OUTPUT_FILE_xlsx.csv', 'w') as csvfile:
   writer = csv.writer(csvfile)
   for root, dirs, files in os.walk(path):
     for filename in files:
